0ea4064e1567d049204d41344d6db75d56618df3
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-10-30 12:35:07 -0700

    Puth modeling loading into _init_

--- Surf_counter/model_server.py
+++ Surf_counter/model_server.py
@@ -4,34 +4,35 @@ from os import listdir
 from os.path import isfile, join
 import os, shutil 
 import urllib.request
+from s3pushpull import s3pushpull
 
 class ServeModel:
-    #model_path = "./models/yolo.h5"
-    model_path = "./models/yolo-tiny.h5"
-        
-    output_path = "./output/breakwaterFull.jpg"
-    print("reading yolo model")
-    #url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
-    url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5'
-    
-    urllib.request.urlretrieve(url, model_path)
-    print("Done reading model")
-
-    print("Loading models")
-    detector = ObjectDetection()
-    detector.setModelTypeAsYOLOv3()
-    detector.setModelTypeAsTinyYOLOv3()
-    detector.setModelPath(model_path)
-    detector.loadModel()
-    print("Done Loading Model")
 
     def __init__(self):
         self.surfimages = ['./data/breakwater/'+ f for f in listdir('./data/breakwater') if f[0] !='.' and isfile(join('./data/breakwater', f))]
+        #model_path = "./models/yolo.h5"
+        self.model_path = "./models/yolo-tiny.h5"
+            
+        self.output_path = "./output/breakwaterFull.jpg"
+        print("reading yolo model")
+        #url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+        self.url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5'
+        urllib.request.urlretrieve(self.url, self.model_path)
+        print("Loading models")
+        self.detector = ObjectDetection()
+        self.detector.setModelTypeAsYOLOv3()
+        self.detector.setModelTypeAsTinyYOLOv3()
+        self.detector.setModelPath(self.model_path)
+        self.detector.loadModel()
+        print("Done Loading Model")
     
     def serveit(self):
         self.surfimages = ['./data/breakwater/'+ f for f in listdir('./data/breakwater') if f[0] !='.' and isfile(join('./data/breakwater', f))]
         detection_ = self.detector.detectObjectsFromImage(input_image=self.surfimages[0],output_image_path=self.output_path,
         minimum_percentage_probability=30)
+
+        # s3=s3pushpull()
+        # d=list(s3.get_matching_s3_keys(prefix='S3:/data/breakwater'))
 
         not_allowed=['airplane','bicycle']
         count=0
