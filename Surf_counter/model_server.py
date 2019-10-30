43a74a90d8b8de6520ef3a6b78a8e00ed333c34f
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-10-30 11:51:40 -0700

    Change to tiny model - memory

--- Surf_counter/model_server.py
+++ Surf_counter/model_server.py
@@ -6,24 +6,29 @@ import os, shutil
 import urllib.request
 
 class ServeModel:
-    model_path = "./models/yolo.h5"
+    #model_path = "./models/yolo.h5"
+    model_path = "./models/yolo-tiny.h5"
+        
     output_path = "./output/breakwaterFull.jpg"
     print("reading yolo model")
-    url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+    #url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+    url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5'
+    
     urllib.request.urlretrieve(url, model_path)
     print("Done reading model")
+
+    print("Loading models")
+    detector = ObjectDetection()
+    #detector.setModelTypeAsYOLOv3()
+    detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelPath(model_path)
+    detector.loadModel()
+    print("Done Loading Model")
 
     def __init__(self):
         self.surfimages = ['./data/breakwater/'+ f for f in listdir('./data/breakwater') if f[0] !='.' and isfile(join('./data/breakwater', f))]
     
     def serveit(self):
-        print("Loading models")
-        detector = ObjectDetection()
-        detector.setModelTypeAsYOLOv3()
-        detector.setModelPath(self.model_path)
-        detector.loadModel()
-
-        print("Done")
         self.surfimages = ['./data/breakwater/'+ f for f in listdir('./data/breakwater') if f[0] !='.' and isfile(join('./data/breakwater', f))]
         detection_ = self.detector.detectObjectsFromImage(input_image=self.surfimages[0],output_image_path=self.output_path,
         minimum_percentage_probability=30)
--- Surf_counter/use_server.py
+++ Surf_counter/use_server.py
@@ -7,3 +7,12 @@ import urllib.request
 
 #url = "https://cfmodelserver.herokuapp.com/model"
 #urllib.request.urlopen('http://python.org/') as response
+
+
+from imageai.Detection import ObjectDetection
+model_path = "./models/yolo-tiny.h5"
+detector = ObjectDetection()
+#detector.setModelTypeAsYOLOv3()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(model_path)
+detector.loadModel()
