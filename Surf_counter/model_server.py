ea3185f688010cafcf745f7741a953978f8a90b4
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-10-30 10:44:56 -0700

    Working locally

---
+++ Surf_counter/model_server.py
@@ -0,0 +1,38 @@
+from imageai.Detection import ObjectDetection
+#import imageai
+from os import listdir
+from os.path import isfile, join
+import os, shutil 
+import urllib.request
+
+url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+urllib.request.urlretrieve(url, 'models/yolo.h5')
+
+class ServeModel:
+    model_path = "./models/yolo.h5"
+    output_path = "./output/breakwaterFull.jpg"
+
+    url = 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'
+    urllib.request.urlretrieve(url, model_path)
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(model_path)
+    detector.loadModel()
+
+    def __init__(self):
+        self.surfimages = ['./data/breakwater/'+ f for f in listdir('./data/breakwater') if f[0] !='.' and isfile(join('./data/breakwater', f))]
+    
+    def serveit(self):
+        self.surfimages = ['./data/breakwater/'+ f for f in listdir('./data/breakwater') if f[0] !='.' and isfile(join('./data/breakwater', f))]
+        detection_ = self.detector.detectObjectsFromImage(input_image=self.surfimages[0],output_image_path=self.output_path,
+        minimum_percentage_probability=30)
+
+        not_allowed=['airplane','bicycle']
+        count=0
+        for x in detection_:
+            if x['name'] not in not_allowed:
+                count=count+1
+
+        return str(count)
+
--- api.py
+++ api.py
@@ -1,65 +1,22 @@
 import flask
 from flask import request, jsonify
 from flask import send_file
-from os import listdir
-from os.path import isfile, join
-import os, shutil
-#from s3pushpull import s3pushpull
-#ddd
-from Surf_counter.detector import Detect
-from Surf_counter.spot_urls import SpotUrls
-#from Surf_counter.scrape_video_links import ScrapeVideoLinks
-#FF
+from Surf_counter.model_server import ServeModel
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-det=Detect()
-#det.pull_images_s3(SpotUrls.venice_static)
+sv=ServeModel()
 
 @app.route('/model')
 def api_model():
-    return (15)
-    #det.clear_data_dir()
-    #Find the video
-    #url=SpotUrls.lookup['venice_beach']
-    #v=ScrapeVideoLinks(url)
-    #link=v.get_link()
-    #det.pull_images_s3(SpotUrls.venice_static)
-    #print("Made it herrrrre")
-    #n_surfers=det.detection()
-    #print ("THERE ARE N SURFERS ", n_surfers)
-    #return str(n_surfers)
-    #return str(23)
+    count=sv.serveit()
+    return count
     
-    #output=f'''There are currently {surfer_count} surfers at the Breakwater'''
-    #return jsonify(n_surfers)
-
-
-
-# s3=s3pushpull()
-# d=list(s3.get_matching_s3_keys(prefix='S3:/data/'))
-# print("List of keys:")
-# print(d)
-# pics = [p for p in d if '.jpg' in p]
-# print('pics')
-# print(pics)
-# print ("downloading")
-# s3.download_aws('MyPic.jpg', pics[0])
-
-# @app.route('/get_image')
-# def get_image():
-#     if request.args.get('type') == '1':
-#         filename = 'data/MyPic.jpg'
-#     else:
-#         filename = 'MyPic.jpg'
-#     return send_file(filename, mimetype='image/jpg')
-
-
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>Surfing a Deep Learning Model</h1>
+<p>A Yolo Model for detecting surfers.</p>'''
 
 
 if __name__ == '__main__':
